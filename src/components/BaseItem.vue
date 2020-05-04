8cfb4844e2ce09f3de03cd35ef798a864b5fdc73
Gabriel Campos <gcampos@id.uff.br>
2020-05-04 16:49:26 -0300

    Mount Page

---
+++ src/components/BaseItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="base-item">
+      <div class="item">
+          <div class="float-img">
+          </div>
+          <div class="item-box">
+              <div class="item-box-header">
+                  <p class="title">Pastel sabor Carne</p>
+                  <p class="value">R$10,00</p>
+              </div>
+              <div class="item-box-body">
+                  <p>Sabor: <span>PowerPoint importante</span></p>
+                  <p>Descrição: <span>1 PPT, 6834 slides e uma pitada de animação</span></p>
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // name: 'HelloWorld',
+  // props: {
+  //   msg: String
+  // }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
